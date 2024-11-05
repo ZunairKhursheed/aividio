4969b5e4f6bed4d173bd3e51f68e31ad77f567b2
ZunairKhursheed <zunairkhursheed97@gmail.com>
2024-11-05 12:15:51 +0500

    first commit

--- pages/api/video_story/[id].js
+++ pages/api/video_story/[id].js
@@ -9,22 +9,26 @@ export default async function handler(req, res) {
     }
   );
   if (!response.ok) {
-      // If the response is not ok (status not in the range 200-299)
-      const error = await response.json();
-      res.status(500).json({ detail: error.detail || 'An error occurred while fetching the prediction.' });
-      return;
-    }
-
-    if (response.status === 401) {
-      res.status(401).json({ message: "You are not authorized. Please log in and try again." });
-      return;
-    }
-
-    const prediction = await response.json();
-    res.status(200).json(prediction);
-  } catch (error) {
-    // Catch any unexpected errors that occur during the fetch
-    console.error("Error fetching prediction:", error);
-    res.status(500).json({ detail: 'An unexpected error occurred. Please try again later.' });
+    // If the response is not ok (status not in the range 200-299)
+    const error = await response.json();
+    res
+      .status(500)
+      .json({
+        detail:
+          error.detail || "An error occurred while fetching the prediction.",
+      });
+    return;
   }
+
+  if (response.status === 401) {
+    res
+      .status(401)
+      .json({
+        message: "You are not authorized. Please log in and try again.",
+      });
+    return;
+  }
+
+  const prediction = await response.json();
+  res.status(200).json(prediction);
 }
